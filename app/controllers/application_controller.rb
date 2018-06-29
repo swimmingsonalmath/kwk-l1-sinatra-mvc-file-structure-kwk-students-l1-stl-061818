a2dc35ae4681884408c2b740df68f32b36aa18ab
Sonali Sharma <sonalisharma58521@gmail.com>
2018-06-29 15:01:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+require ',/config/enviroment'
+require ',/app/models/model'
+
+
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -5,23 +9,27 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do    #this is your default/homepage(get'/')
-  	erb :index
-  end
-get"/newpage"do
-  erb :new
-end 
+#   get "/" do    #this is your default/homepage(get'/')
+#   	erb :index
+#   end
+# get"/newpage"do
+#   erb :new
+# end 
 
-get '/dogs'do
-  first_dog = Dog.new("Clifford","Sammy","cat")
-first_dog.get_name  
+# get '/dogs'do
+#   first_dog = Dog.new("Clifford","Sammy","cat")
+# first_dog.get_name  
   
-end
-
-
-
-
-
+# end
+ get'/'do 
+   return erb:index
+ end
+ 
+ post'/'do    #we click the butto now what
+   @the_fortune=get_fortune   #method get_fortune is connected to the instance variable @the_fortune
+   @the_user=params[:user] #params is a has that stores user input this line is also creating  a new varable user 
+   return erb :results
+ end
 
 end
 
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,15 @@
-class Model
-  # Replace with Dog clas
-end
+# class Model
+#   # Replace with Dog clas
+# end
+
+def get_fortune
+  
+ fortune_array = ["Your water bottle will fly today","You will get a nosebleed on the Metro","You will have an amazing lunch","You will skip breakfast","You will drink 30 core water bottles this week","You will fall in a hole in 2 days","You will find your left shoe this afternoon","eat bok choy",]
+ return fortune_array.sample
+ #fortune = fortune_array.sample
+# fortune_array.delete(fortune)       #for deleting fortunes after used
+ #return fortune
+ 
+ end
+ 
+# puts get_fortune                         for ruby unhash this and then it works in the terminal
